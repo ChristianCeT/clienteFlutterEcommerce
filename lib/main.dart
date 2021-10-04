dd636496d812c9d84e91410f792e9f57abb7ce9d
ChristianCeT <chrisatnat@hotmail.com>
2021-10-04 02:45:29 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:app_test/src/login/login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Delivery App',
+      initialRoute: "login",
+      routes: {'login': (BuildContext context) => const LoginPage()},
+    );
+  }
+}
